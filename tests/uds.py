8941cb52931f1c27909b51b2bbb0c1f6b79bc617
Gabriel Vian <g@gabrielvian.com>
2018-12-11 18:11:30 +0100

    added average

--- tests/uds.py
+++ tests/uds.py
@@ -7,26 +7,30 @@ echo = InputDevice(17)
 sleep(2)
 
 def get_pulse_time():
-	trig.on()
-	sleep(0.00001)
-	trig.off()
+    trig.on()
+    sleep(0.00001)
+    trig.off()
 
-	while echo.is_active == False:
-		pulse_start = time()
+    while echo.is_active == False:
+        pulse_start = time()
 
-	while echo.is_active == True:
-		pulse_end = time()
+    while echo.is_active == True:
+        pulse_end = time()
 
-	sleep(0.06)
+    sleep(0.1) # 0.06
 
-	return pulse_end - pulse_start
+    return pulse_end - pulse_start
 
 def calculate_distance(duration):
-	speed = 343
-	distance = speed * duration / 2
-	return distance
+    speed = 343
+    distance = speed * duration / 2
+    return distance
 
 while True:
-	duration = get_pulse_time()
-	distance = calculate_distance(duration)
-	print(distance)
+    avg = 0
+    for i in range(10):
+        duration = get_pulse_time()
+        distance = calculate_distance(duration)
+        avg += distance
+        
+    print(avg/5)
