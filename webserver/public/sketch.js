2b5aeaff108f5fcf39bb944ec2e5dea8c7225cd8
Gabriel Vian <g@gabrielvian.com>
2018-12-14 23:03:19 +0100

    stable version

--- webserver/public/sketch.js
+++ webserver/public/sketch.js
@@ -1,47 +1,56 @@
-currentRotation = 0
+let currentRotation = 0
+let obstacles = []
+let max = 1500
 
 function setup() {
-	angleMode(DEGREES)
 	createCanvas(900, 900)
+	angleMode(DEGREES)
 	translate(width/2, height/2)
-	rotate(180)
+	rotate(-90)
 }
 
 function draw() {
 	background(0)
-	transformRadar()	// Crea piano cartesiano mettendo l'origine al centro e ruotando di 180°
-	drawRadar()			// Disegna circonferenza e indicatore
-	drawObstacles()
-	rotateRadar()		// Ruota l'indicatore di n gradi
-}
 
-
-
-function rotateRadar() {
-	currentRotation += 1
-}
-
-function drawRadar() {
+	translate(width/2, height/2)
+	rotate(-90)
+	
 	noFill()
 	stroke(255)
-	strokeWeight(5)
+	strokeWeight(10)
 	point(0, 0)
 	strokeWeight(2)
 	ellipse(0, 0, width, height)
-	line(0, 0, getCoords(currentRotation, width/2)[0], getCoords(currentRotation, height/2)[1])
-}
+	line(0, 0, getCoords(currentRotation, width/2).x, getCoords(currentRotation, width/2).y)
 
-function transformRadar() {
-	translate(width / 2, height / 2)
-	rotate(180)
+	//obstacles = getObstacles()
+
+	for (let i = 0; i < obstacles.length; i++) {
+		obstacles[i].draw()
+		if (obstacles[i].check(currentRotation)) {
+			obstacles.splice(i, 1)
+		}
+	}
+
+	currentRotation += 1
+
+	if (mouseIsPressed) {
+		obstacles.push(new Obstacle(currentRotation, random(70, width/2)))
+	}
 }
 
 function getCoords(deg, dist) {
 	let x = Math.cos(deg * Math.PI/180) * dist
 	let y = Math.sin(deg * Math.PI/180) * dist
-	return [x, y]
+	return createVector(x, y)
 }
 
-function drawObstacles() {
+function transCoords(x, y) {
+	x = x - width / 2
+	y = -1 * y - height / 2
+	return createVector(x, y)
+}
+
+function getObstacles() {
 	
 }
